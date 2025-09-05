cf45962e390621271482c5f0d1bda73fc9c62d1f
Mohamed Toujani <132010255+1MrazorT1@users.noreply.github.com>
2025-09-05 13:58:14 +0200

    Update classes.py

--- TP1/classes.py
+++ TP1/classes.py
@@ -1,1 +1,8 @@
-
+class Group(object):
+  def __init__(self, l, e, N, p):
+    self.l = l
+    self.e = e
+    self.N = N
+    self.p = p
+    if self.checkParameters() != True:
+      raise Exception("Problem with parameters")
