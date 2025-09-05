62f19ba8bbace41606be70477e859c1fc45cd9f6
Mohamed Toujani <1mohamedtoujani1@gmail.com>
2025-09-05 14:25:23 +0100

    fixing through tests

--- TP1/classes.py
+++ TP1/classes.py
@@ -1,3 +1,4 @@
+import math
 class Group(object):
   def __init__(self, l, e, N, p):
     self.l = l
@@ -8,30 +9,31 @@ class Group(object):
       raise Exception("Problem with parameters")
   
   def checkParameters(self):
-    if self.l == null:
+    if self.l == "":
       raise Exception("l is unknown.")
-    return (self.l == "ZpAdditive" and self.e == 0) or (self.l == "ZpMultiplicative") and self.e == 1)
+    return (self.l == "ZpAdditive" and self.e == 0) or (self.l == "ZpMultiplicative" and self.e == 1)
 
   def law(self, g1, g2):
     if self.l == "ZpAdditive":
-      return (g1 + g2) % p
-    else return (g1 * g2) % p
+      return (g1 + g2) % self.p
+    else:
+      return (g1 * g2) % self.p
 
   def exp(self, g, k):
     if k == 0:
       return self.e
     elif k == -1:
-      exp(g, N-1)
+      return self.exp(g, self.N-1)
     else:
       h0 = self.e
       h1 = g
-      t = floor(log(k))
-      for i in range(t -1, -1, -1):
-        ki = (h1 >> i) & 1
+      t = math.floor(math.log2(k))
+      for i in range(t, -1, -1):
+        ki = (k >> i) & 1
         if ki == 0:
-          h1 = law(h0, h1)
-          h0 = law(h0, h0)
+          h1 = self.law(h0, h1)
+          h0 = self.law(h0, h0)
         else:
-          h0 = law(h0, h1)
-          h1 = law(h1, h1)
+          h0 = self.law(h0, h1)
+          h1 = self.law(h1, h1)
       return h0
--- TP1/tests.py
+++ TP1/tests.py
@@ -1,7 +1,11 @@
+from classes import Group
+
 def testLab1_part1():
   monGroupe = Group("ZpAdditive", 0, 23, 23)
   print("In Z23 : exp(5,7) = 12 ?", monGroupe.exp(5,7) == 12)
   Z23_mult = Group("ZpMultiplicative", 1, 22, 23)
   print("In (Z23)* : exp(5,7) = 17 ?", Z23_mult.exp(5,7) == 17)
   print("In Z23 : exp(5,-1) = 18 ?", monGroupe.exp(5,-1) == 18)
-  print("In (Z23)* : exp(5,-1) = 14 ?", monGroupe.exp(5,-1) == 14)
+  print("In (Z23)* : exp(5,-1) = 14 ?", Z23_mult.exp(5,-1) == 14)
+
+testLab1_part1()
