f38e342a79ae456ae2f6585574644d149c48a787
Mohamed Toujani <1mohamedtoujani1@gmail.com>
2025-09-26 14:42:56 +0200

    mid checkHeader

--- TP4/fonctions.py
+++ TP4/fonctions.py
@@ -1,3 +1,5 @@
+from json import load
+
 def is_on_X25519_Euler_criteria(u, p, A):
     x = ((u**3 % p) + A * (u**2 % p) + u) % p
     return x == 0 or pow(x, (p - 1) // 2, p) == 1
@@ -9,3 +11,14 @@ def reverseBytes(data):
     data = bytearray(data)
     data.reverse()
     return data
+
+def recoverData(Filename):
+    with open(Filename, "r") as f:
+        data = load(f)
+    id_block = list(data["data"].keys())[0]
+    block_header = data["data"][id_block]["raw_block"][:160]
+    return block_header
+
+def checkHeader():
+    print()
+
